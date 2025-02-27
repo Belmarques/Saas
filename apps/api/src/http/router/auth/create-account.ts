5a5e9a353e8a6665d5348dd97126082d03091699
Belmarques <gabrielll0701@gmail.com>
2025-02-27 19:23:24 -0300

    Criando rota de cadastro

--- apps/api/src/http/router/auth/create-account.ts
+++ apps/api/src/http/router/auth/create-account.ts
@@ -1,21 +1,43 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
     {
       schema: {
+        tags: ['auth'],
+        summary: 'create a new account',
         body: z.object({
-          nome: z.string(),
+          name: z.string(),
           email: z.string().email(),
           password: z.string().min(6),
         }),
       },
     },
-    () => {
-      return 'user create'
+    async (request, reply) => {
+      const { email, name, password } = request.body
+      const userWhithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+      if (userWhithSameEmail) {
+        return reply.status(400).send({ message: 'user e-mail already exists' })
+      }
+      const passwordHash = await hash(password, 6)
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+      return reply.status(201).send()
     },
   )
 }
