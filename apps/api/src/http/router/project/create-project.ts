80b2afcd921400fdae636f1ba5e9b29b0a1c7ea4
Belmarques <gabrielll0701@gmail.com>
2025-03-08 00:13:16 -0300

    criando project

--- apps/api/src/http/router/project/create-project.ts
+++ apps/api/src/http/router/project/create-project.ts
@@ -17,7 +17,7 @@ export async function createProject(app: FastifyInstance) {
       '/organizations/:slug/projects',
       {
         schema: {
-          tags: ['Projects'],
+          tags: ['projects'],
           summary: 'Create a new project',
           security: [{ bearerAuth: [] }],
           body: z.object({
